d6c5b1eafd28b675d96b47f90be63ec49f88b7a9
Pedro Souto <148163288+Loosei@users.noreply.github.com>
2024-03-21 13:38:32 -0300

    Update lista1_1.cpp

--- poo/lista1_1.cpp
+++ poo/lista1_1.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+class AgendaTelefonica {
+private:
+    struct Contato {
+        string nome;
+        string telefone;
+    };
+
+    vector<Contato> contatos;
+
+public:
+    void adicionarContato(const string& nome, const string& telefone) {
+        Contato novoContato = {nome, telefone};
+        contatos.push_back(novoContato);
+    }
+
+    void imprimirAgenda() const {
+        cout << "Agenda Telefônica:\n";
+        for (const auto& contato : contatos) {
+            cout << "Nome: " << contato.nome << ", Telefone: " << contato.telefone << endl;
+        }
+    }
+};
+
+int main() {
+    AgendaTelefonica agenda;
+    agenda.adicionarContato("João", "123456789");
+    agenda.adicionarContato("Maria", "987654321");
+    agenda.adicionarContato("Pedro", "999888777");
+    agenda.imprimirAgenda();
+    return 0;
+}
 
